02f85e1fe4e21fc4d48871819ecf900232497717
halcharger <allen.firth@gmail.com>
2014-03-27 14:19:21 +0000

    pulling in Microsoft.AspNet.WebApi.OwinSelfHost nuget package

--- service/Properties/AssemblyInfo.cs
+++ service/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aaad8a81-b9e2-471b-b965-900b7f8f5e55")]
+[assembly: Guid("858dc8e3-79da-4a9e-a259-7ea5bebb36e4")]
 
 // Version information for an assembly consists of the following four values:
 //
